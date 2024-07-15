9b7b374d57dd2955bee7b86c43ad242db929615c
lukas-laas <lukas.s.laas@gmail.com>
2024-07-15 10:52:08 +0200

    render based on search

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import { getFiltered } from "./search";
 function App() {
   const [search, setSearch] = useState<string | null>(null);
+  const [input, setInput] = useState("");
+  const [results, setResults] = useState<any>(null);
+  useEffect(() => {
+    getFiltered(search ? search : "", setResults);
+  }, [search]);
 
   return (
     <>
-      <form action="">
-        <input type="text" />
-        <button>Search</button>
+      <form
+        action=""
+        onSubmit={(e) => {
+          e.preventDefault();
+          setSearch(input);
+        }}
+      >
+        <input type="text" onChange={(e) => setInput(e.target.value)} />
+        <button type="submit">Search</button>
       </form>
       <ul>
-        <li>
-          <h3>Title</h3>
-          <p>Content</p>
-        </li>
+        {results &&
+          results.map((result) => {
+            return (
+              <li key={result.title}>
+                <h3>{result.title}</h3>
+                <p>{result.content}</p>
+              </li>
+            );
+          })}
       </ul>
     </>
   );
